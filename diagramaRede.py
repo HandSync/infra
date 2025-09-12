5081b2d1fd0c297bd2aedd44baf363672793f152
MarcelaGTO <marcelaagteixeira@gmail.com>
2025-09-12 14:52:46 -0300

    update diagrams

--- diagramaRede.py
+++ diagramaRede.py
@@ -1,29 +1,32 @@
 from diagrams import Diagram, Cluster, Edge
 from diagrams.aws.network import VPC, InternetGateway, RouteTable, ELB
 from diagrams.aws.compute import EC2
-# from diagrams.aws.database import
 from diagrams.aws.storage import S3, EBS
 from diagrams.aws.management import Cloudwatch
 from diagrams.onprem.client import Users
 
 with Diagram(
-    "Arquitetura AWS HandSync",
     outformat="png",
     show=True,
     filename="Arquitetura de Redes - HandSync",
+    graph_attr={"label": "<<B>Arquitetura de Redes - HandSync</B>>", "labelloc": "t", "fontsize": "20"}
     ):
+
     Users = Users("User")
-    # igw = InternetGateway("Internet Gateway")
 
-    # client >> igw
-
-    with Cluster("AWS - Rede Externa", graph_attr={"bgcolor": "#f7ebda"}):
-
+    with Cluster("<<B>AWS - Rede Externa</B>>", graph_attr={"bgcolor": "#f7ebda"}):
+        
         igw = InternetGateway("Internet Gateway")
         rt = RouteTable("Route Table")
         Users >> igw
 
-        with Cluster("VPC - Rede principal", graph_attr={"bgcolor": "#f5dab4ce"}):
+        with Cluster("<<B>Bucket S3</B>>", graph_attr={"style": "dashed"}):
+
+            s3_raw = S3("<<B>RAW</B>>")
+            s3_trusted = S3("<<B>TRUSTED</B>>")
+            s3_curated = S3("<<B>CURATED</B>>")
+
+        with Cluster("<<B>VPC - Rede principal</B>>", graph_attr={"bgcolor": "#f5dab4ce"}):
 
             cloudWatch = Cloudwatch("CloudWatch")
             vpc = VPC("VPC")
@@ -52,28 +55,16 @@ with Diagram(
                     be2 = EC2("Instância Back-end")
                     ebs2 = EBS("EBS")
 
-
-            
-        # # elb_public >> [fe1, fe2]
-        be1 >> ebs1
-        be2 >> ebs2
-        ebs1 >> be2
-        fe1 >> ebs3
-        
-        fe2 >> ebs4
-        ebs3 >> fe2
-        # ebs1 >> elb_private
-        # ebs2 >> elb_private
-        elb_private >> ebs1
-        elb_private >> be2
-        elb_public >> ebs3
-        elb_public >> fe2
-        # elb_public >> elb_private
-        vpc >> [be1, fe1]
-        # fe1 >> be1 >> ebs1
-        # fe2 >> be2 >> ebs2
-        # # s3 = S3("Bucket S3")
-        # # ebs1 >> s3
-        # # ebs2 >> s3
+    be1 >> ebs1
+    be2 >> ebs2
+    ebs1 >> be2
+    fe1 >> ebs3
+    fe2 >> ebs4
+    ebs3 >> fe2
+    elb_private >> ebs1
+    elb_private >> be2
+    elb_public >> ebs3
+    elb_public >> fe2
+    vpc >> Edge(color="purple")>>[be1, fe1]
 
     
